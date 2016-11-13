90ce1014f4ecf0b9951614ef4811ee390c8895d2
Pavel Ershov <devnulpavel@gmail.com>
2016-11-13 17:56:41 +0300

    Files path fix

--- make_samples_for_pangaea.sh
+++ make_samples_for_pangaea.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-FOLDER_NAME=Bank
+RESULTS_FOLDER=Results
+BANK_FOLDER_NAME=Bank
 MAX_PRESETS=10
-MAX_FILES=10
+MAX_BANKS=10
 MAX_SAMPLE_LENGTH=20 # NOTE: In milliseconds from 1 to 999. CP-100 cuts it at 20.5ms anyway.
 
 FILES=(*.wav)
@@ -15,31 +16,31 @@ function convert_file {
 
 function create_preset {
     local file=$1
-    local preset_name=Preset_$2
-    local preset_folder_name=$FOLDER_NAME/$preset_name
-    local sample_name=$preset_folder_name/$file
+    local bank_folder_name="$(printf "%s_%s" $BANK_FOLDER_NAME $2)"
+    local preset_folder_name=Preset_$3
 
-    mkdir -p $preset_folder_name
-    convert_file "$file" "$sample_name"
+    mkdir -p "$RESULTS_FOLDER/$bank_folder_name/$preset_folder_name"
+    convert_file "$file" "$RESULTS_FOLDER/$bank_folder_name/$preset_folder_name/$file"
 }
 
-rm -rf $FOLDER_NAME
+# Remove old results
+rm -rf $RESULTS_FOLDER
 
-folderCounter=0
-processedFiles=0
+banksCounter=0
+presetsCounter=0
 for ((i=0; i < ${#FILES[@]}; i++)); do
-    if [ $processedFiles -ge $MAX_FILES ] ; then 
-        let "folderCounter = $folderCounter + 1"; 
-        processedFiles=0; 
+    if [ $presetsCounter -ge $MAX_PRESETS ] ; then 
+        let "banksCounter = $banksCounter + 1";
+        presetsCounter=0;
     fi
-    if [ $folderCounter -ge $MAX_PRESETS ] ; then 
+    if [ $banksCounter -ge $MAX_BANKS ] ; then 
         break; 
     fi
 
     FILE=${FILES[i]}
-    create_preset "${FILE}" "$folderCounter"
+    create_preset "${FILE}" "$banksCounter" "$presetsCounter"
 
-    let "processedFiles = $processedFiles + 1"
+    let "presetsCounter = $presetsCounter + 1"
 
-    echo "Processed \"$FILE\" as Preset #$folderCounter"
+    echo "Processed \"$FILE\" as Bank #$banksCounter, Preset #$presetsCounter"
 done
