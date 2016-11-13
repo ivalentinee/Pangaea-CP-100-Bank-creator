825d17148afc76b95cf0a5d6a598de456a2a1799
Emperor <valentine.emperor@gmail.com>
2016-11-13 19:51:21 +0400

    add default config file copying for preset

--- make_samples_for_pangaea.sh
+++ make_samples_for_pangaea.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-RESULTS_FOLDER=Result
-BANK_FOLDER_NAME=Bank
+SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CONFIG_FILE_NAME=preset.pan
+CONFIG_FILE_PATH="$SCRIPT_DIRECTORY/$CONFIG_FILE_NAME"
+RESULTS_DIRECTORY=Result
+BANK_DIRECTORY_NAME=Bank
 MAX_PRESETS=10
 MAX_BANKS=10
 MAX_SAMPLE_LENGTH=20 # NOTE: In milliseconds from 1 to 999. CP-100 cuts it at 20.5ms anyway.
@@ -14,17 +17,22 @@ function convert_file {
     sox "$1" -t wavpcm -b 24 -e signed-integer -r 48000 -c 1 "$2" trim 0 00:00:00.$mSecLength
 }
 
+function add_config_file {
+    cp -f $CONFIG_FILE_PATH "$1/$CONFIG_FILE_NAME"
+}
+
 function create_preset {
     local file=$1
-    local bank_folder_name="$(printf "%s_%s" $BANK_FOLDER_NAME $2)"
-    local preset_folder_name=Preset_$3
+    local bank_directory_name="$(printf "%s_%s" $BANK_DIRECTORY_NAME $2)"
+    local preset_directory_name=Preset_$3
 
-    mkdir -p "$RESULTS_FOLDER/$bank_folder_name/$preset_folder_name"
-    convert_file "$file" "$RESULTS_FOLDER/$bank_folder_name/$preset_folder_name/$file"
+    mkdir -p "$RESULTS_DIRECTORY/$bank_directory_name/$preset_directory_name"
+    convert_file "$file" "$RESULTS_DIRECTORY/$bank_directory_name/$preset_directory_name/$file"
+    add_config_file "$RESULTS_DIRECTORY/$bank_directory_name/$preset_directory_name/"
 }
 
 # Remove old results
-rm -rf $RESULTS_FOLDER
+rm -rf $RESULTS_DIRECTORY
 
 banksCounter=0
 presetsCounter=0
